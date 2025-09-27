088bffee5b051fbdea6e4165c32c0361a8bde8b7
Chris Johnson <chrisljohnson86@gmail.com>
2025-09-26 18:45:39 -0600

    Code for Prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Series of Numbers by Chris Johnson 9/26/25
+
+
+        //string textFromUser = "10";
+        int numberFromUser = 1;
+        double numberTotal = 0;
+        double numberAverage = 0;
+        decimal numAveFormatted;
+        int numberMaximum = 0;
+        int numberPositiveMinimum = 100;
+        int numberCount = 1;
+        // string continueGame no;
+        List<int> numbers;
+        numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (numberFromUser != 0)
+        {
+            Console.Write("Enter number:");
+            string textFromUser = Console.ReadLine();
+            numberFromUser = int.Parse(textFromUser);
+            numbers.Add(numberFromUser);
+            numberCount = numberCount + 1;
+            numberTotal = numberTotal + numberFromUser;
+
+            if (numberMaximum < numberFromUser)
+            {
+                numberMaximum = numberFromUser;
+            }
+            if (numberFromUser > 0)
+            {
+                if (numberPositiveMinimum > numberFromUser)
+                {
+                    numberPositiveMinimum = numberFromUser;
+                }
+
+            }
+            
+            numbers.Remove(0);
+
+        }
+        numberAverage = (numberTotal / (numberCount - 2));
+        decimal numAveTemp = (decimal)numberAverage;
+        numAveFormatted = Decimal.Round(numAveTemp, 4);
+        Console.WriteLine($"The sum is: {numberTotal}");
+        Console.WriteLine($"The average is: {numAveFormatted}");
+        Console.WriteLine($"The largest number is: {numberMaximum}");
+        Console.WriteLine($"The smallest positive number is: {numberPositiveMinimum}");
+
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
     }
 }
